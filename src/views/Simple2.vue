b41e1ffcee06393d83233a8c28b89988278eca5c
Pab <pabrock12345@gmail.com>
2024-05-13 11:53:31 +0700

    update login simple2

--- src/views/Simple2.vue
+++ src/views/Simple2.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
     <h1>Welcome to the login page</h1>
-    <v-flex>
-        <v-text-field>
-        </v-text-field>
+    <v-flex xs4>
+      <v-text-field
+      v-model="email"
+      label="E-Mail"
+      prepend-icon="mdi-email"
+      id="email"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs4>
+        <v-text-field
+        v-model="password"
+        label="Password"
+        prepend-icon="mdi-lock"
+        id="password"
+        ></v-text-field>
     </v-flex>
   </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signin () {
+      console.log(this.email)
+    }
+  }
 }
 </script>
